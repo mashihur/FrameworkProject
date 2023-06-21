b65e6a0a2c344a35b7f6b5bd4d0a03dfa666d3b7
Khang Nguyen Truong <tnkhang97@gmail.com>
2023-06-21 10:47:28 -0500

    implement deposit and withdraw

--- src/bankingSystem/BankMain.java
+++ src/bankingSystem/BankMain.java
@@ -1,10 +1,11 @@
 package bankingSystem;
 
 import bankingSystem.ui.BankFrame;
+import framework.ui.FinCoFrame;
 
 public class BankMain {
     public static void main(String[] args) {
-        BankFrame bankApp = new BankFrame();
+        FinCoFrame bankApp = new BankFrame();
         bankApp.setTitle("Bank Application");
         bankApp.setVisible(true);
     }
